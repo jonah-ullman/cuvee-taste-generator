60e220ca54291248f2d7b1c8bdd401e67126052b
Jonah Ullman <jonahmullman@gmail.com>
2020-05-03 21:55:21 -0500

    production build

---
+++ build/precache-manifest.d1e401c0c1bd875e9dd5fe9f7353148b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a1e956359600b320b7260e0ed023d4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5eff6223689a802de58",
+    "url": "/static/css/main.7959ac14.chunk.css"
+  },
+  {
+    "revision": "19a807f103f8338fd586",
+    "url": "/static/js/2.ca70520a.chunk.js"
+  },
+  {
+    "revision": "86414f6aa96d78f04460ff2dda206a29",
+    "url": "/static/js/2.ca70520a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5eff6223689a802de58",
+    "url": "/static/js/main.d74131e2.chunk.js"
+  },
+  {
+    "revision": "cb64ac2bb49302dc5701",
+    "url": "/static/js/runtime-main.45063306.js"
+  }
+]);
